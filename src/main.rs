1143ee7ba8961c2e31557405e5d37df07aa3febc
Fábio Corrêa <feamcor@gmail.com>
2025-10-26 12:43:13 -0300

    Implement exit

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,17 @@ fn main() {
         io::stdout().flush().unwrap();
         command.clear();
         match io::stdin().read_line(&mut command) {
-            Ok(_) => {
-                eprintln!("{}: command not found", command.trim());
+            Ok(bytes_read) => {
+                let command = command.trim();
+                if command.starts_with("exit") {
+                    if bytes_read == 5 {
+                        std::process::exit(0);
+                    } else {
+                        let exit_status = command[4..].trim().parse().unwrap_or(0);
+                        std::process::exit(exit_status as i32);
+                    }
+                }
+                eprintln!("{}: command not found", command);
             }
             Err(e) => {
                 eprintln!("Error: {}", e);
