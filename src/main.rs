678a77f1bd70c5dddffd68bb8780d4f09d1722f0
Fábio Corrêa <feamcor@gmail.com>
2025-10-26 21:30:48 -0300

    Implement exit refactored

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,41 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::iter::Enumerate;
+use std::str::SplitWhitespace;
+
+static SHELL_PROMPT: &str = "$";
 
 fn main() {
-    let mut command = String::new();
+    let mut input = String::new();
     loop {
-        print!("$ ");
+        print!("{} ", SHELL_PROMPT);
         io::stdout().flush().unwrap();
-        command.clear();
-        match io::stdin().read_line(&mut command) {
-            Ok(bytes_read) => {
-                let command = command.trim();
-                if command.starts_with("exit") {
-                    if bytes_read == 5 {
-                        std::process::exit(0);
-                    } else {
-                        let exit_status = command[4..].trim().parse().unwrap_or(0);
-                        std::process::exit(exit_status as i32);
-                    }
+        input.clear();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                let input = input.trim();
+                if input.is_empty() { continue; }
+                let mut arguments = input.split_whitespace().enumerate();
+                let (_, command) = arguments.next().unwrap();
+                match command {
+                    "exit" => command_exit(arguments),
+                    _ => eprintln!("{}: command not found", command),
                 }
-                eprintln!("{}: command not found", command);
             }
             Err(e) => {
                 eprintln!("Error: {}", e);
             }
         }
     }
+}
+
+fn command_exit(arguments: Enumerate<SplitWhitespace>) {
+    let mut exit_status = 0;
+    for (index, argument) in arguments {
+        if index == 1 {
+            exit_status = argument.parse().unwrap_or(1);
+            break;
+        }
+    }
+    std::process::exit(exit_status as i32);
 }
