8c4308baa6812b507bb4140bd6164e4937b7a61b
Fábio Corrêa <feamcor@gmail.com>
2025-10-28 15:45:07 -0300

    Redirect stdout

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use core::slice::Iter;
 use std::env::{current_dir, set_current_dir, var};
+use std::fs::{File, OpenOptions};
 use std::io::{self, Write};
 use std::iter::Enumerate;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
 static SHELL_PROMPT: &str = "$ ";
+
+enum OutputType {
+    STDOUT = 1,
+    STDERR = 2,
+}
+
+struct OutputRedirection {
+    file_name: Option<String>,
+    append_to: bool,
+    output_type: OutputType,
+}
+
+struct ParsedCommand {
+    tokens: Option<Vec<String>>,
+    stdout: OutputRedirection,
+    stderr: OutputRedirection,
+}
 
 fn main() {
     let mut input = String::new();
@@ -17,16 +35,19 @@ fn main() {
             Ok(_) => {
                 let input = input.trim();
                 if input.is_empty() { continue; }
-                let tokens = parse_tokens(input);
-                let mut arguments = tokens.iter().enumerate();
+                let parsed_command = parse_command(input);
+                let mut arguments = match &parsed_command.tokens {
+                    Some(tokens) => tokens.iter().enumerate(),
+                    None => continue,
+                };
                 let (_, command) = arguments.next().unwrap();
                 match command.as_str() {
-                    "cd"   => command_cd(arguments),
-                    "echo" => command_echo(arguments),
-                    "exit" => command_exit(arguments),
-                    "pwd"  => command_pwd(arguments),
-                    "type" => command_type(arguments),
-                    _ => run_executable(command, arguments),
+                    "cd"   => command_cd(arguments,          parsed_command.stdout, parsed_command.stderr),
+                    "echo" => command_echo(arguments,        parsed_command.stdout, parsed_command.stderr),
+                    "exit" => command_exit(arguments,        parsed_command.stdout, parsed_command.stderr),
+                    "pwd"  => command_pwd(arguments,         parsed_command.stdout, parsed_command.stderr),
+                    "type" => command_type(arguments,        parsed_command.stdout, parsed_command.stderr),
+                    _ => run_executable(command, arguments,  parsed_command.stdout, parsed_command.stderr),
                 }
             }
             Err(e) => {
@@ -36,7 +57,7 @@ fn main() {
     }
 }
 
-fn command_exit(arguments: Enumerate<Iter<String>>) {
+fn command_exit(arguments: Enumerate<Iter<String>>, _stdout: OutputRedirection, _stderr: OutputRedirection) {
     let mut exit_status = 0;
     for (_index, argument) in arguments.take(1) {
         exit_status = argument.parse().unwrap_or(1);
@@ -44,23 +65,26 @@ fn command_exit(arguments: Enumerate<Iter<String>>) {
     std::process::exit(exit_status);
 }
 
-fn command_echo(arguments: Enumerate<Iter<String>>) {
+fn command_echo(arguments: Enumerate<Iter<String>>, stdout: OutputRedirection, _stderr: OutputRedirection) {
+    let mut stdout = get_output_redirection(stdout);
     for (index, argument) in arguments {
-        if index > 1 { print!(" "); }
-        print!("{argument}");
+        if index > 1 { write!(stdout, " ").unwrap_or_default(); }
+        write!(stdout, "{argument}").unwrap_or_default();
     }
-    println!();
+    writeln!(stdout).unwrap_or_default();
 }
 
-fn command_type(arguments: Enumerate<Iter<String>>) {
+fn command_type(arguments: Enumerate<Iter<String>>, stdout: OutputRedirection, stderr: OutputRedirection) {
+    let mut stdout = get_output_redirection(stdout);
+    let mut stderr = get_output_redirection(stderr);
     for (_index, argument) in arguments.take(1) {
         match argument.as_str() {
             "cd" | "echo" | "exit" | "pwd" | "type" =>
-                println!("{argument} is a shell builtin"),
+                writeln!(stdout, "{argument} is a shell builtin").unwrap_or_default(),
             _ => {
                 match search_executable(argument) {
-                    Some(full_path_to_executable) => println!("{argument} is {full_path_to_executable}"),
-                    None => eprintln!("{argument}: not found")
+                    Some(full_path_to_executable) => writeln!(stdout, "{argument} is {full_path_to_executable}").unwrap_or_default(),
+                    None => writeln!(stderr, "{argument}: not found").unwrap_or_default(),
                 }
             },
         }
@@ -84,27 +108,31 @@ fn is_executable(full_path_to_executable: &PathBuf) -> io::Result<bool> {
     Ok(metadata.permissions().mode() & 0o111 != 0)
 }
 
-fn run_executable(command: &str, arguments: Enumerate<Iter<String>>) {
+fn run_executable(command: &str, arguments: Enumerate<Iter<String>>, stdout: OutputRedirection, stderr: OutputRedirection) {
+    let mut stdout = get_output_redirection(stdout);
+    let mut stderr = get_output_redirection(stderr);
     match search_executable(command) {
         Some(_) => {
             let output = Command::new(command)
                 .args(arguments.map(|(_, argument)| argument))
                 .output();
             match output {
-                Ok(output) => print!("{}", String::from_utf8_lossy(&output.stdout)),
-                Err(e) => eprintln!("{e}"),
+                Ok(output) => write!(stdout, "{}", String::from_utf8_lossy(&output.stdout)).unwrap_or_default(),
+                Err(e) => writeln!(stderr, "{e}").unwrap_or_default(),
             }
         },
-        None => eprintln!("{command}: command not found"),
+        None => writeln!(stderr, "{command}: command not found").unwrap_or_default(),
     }
 }
 
-fn command_pwd(_arguments: Enumerate<Iter<String>>) {
+fn command_pwd(_arguments: Enumerate<Iter<String>>, stdout: OutputRedirection, _stderr: OutputRedirection) {
+    let mut stdout = get_output_redirection(stdout);
     let current_directory = current_dir().unwrap();
-    println!("{}", current_directory.to_string_lossy());
+    writeln!(stdout, "{}", current_directory.to_string_lossy()).unwrap_or_default();
 }
 
-fn command_cd(arguments: Enumerate<Iter<String>>) {
+fn command_cd(arguments: Enumerate<Iter<String>>, _stdout: OutputRedirection, stderr: OutputRedirection) {
+    let mut stderr = get_output_redirection(stderr);
     let home_directory = var("HOME").unwrap_or(String::new());
     let mut directory: &str = "";
     for (_index, argument) in arguments.take(1) {
@@ -116,22 +144,27 @@ fn command_cd(arguments: Enumerate<Iter<String>>) {
     directory = if directory.is_empty() { &home_directory } else { directory };
     match set_current_dir(directory) {
         Ok(_) => (),
-        Err(_) => eprintln!("cd: {directory}: No such file or directory"),
+        Err(_) => writeln!(stderr, "cd: {directory}: No such file or directory").unwrap_or_default(),
     }
 }
 
-fn parse_tokens(input: &str) -> Vec<String> {
+fn parse_command(input: &str) -> ParsedCommand {
     let mut tokens = Vec::new();
+    let mut stdout: OutputRedirection = OutputRedirection { file_name: None, append_to: false, output_type: OutputType::STDOUT };
+    let mut stderr: OutputRedirection = OutputRedirection { file_name: None, append_to: false, output_type: OutputType::STDERR };
+
     let mut current_token = String::new();
     let mut in_single_quotes = false;
     let mut in_double_quotes = false;
-    let mut escape_next_character = false;
+    let mut escape_next_char = false;
+    let mut in_stdout_redirection = false;
+    let mut in_stderr_redirection = false;
 
     let mut characters = input.trim().chars().peekable();
 
     while let Some(character) = characters.next() {
         match character {
-            '\'' if !escape_next_character => {
+            '\'' if !escape_next_char => {
                 if current_token.is_empty() {
                     in_single_quotes = true;
                     in_double_quotes = false;
@@ -148,7 +181,7 @@ fn parse_tokens(input: &str) -> Vec<String> {
                     }
                 }
             },
-            '"' if !escape_next_character => {
+            '"' if !escape_next_char => {
                 if current_token.is_empty() {
                     in_single_quotes = false;
                     in_double_quotes = true;
@@ -165,36 +198,106 @@ fn parse_tokens(input: &str) -> Vec<String> {
                     }
                 }
             },
-            '\\' if !escape_next_character => {
+            '\\' if !escape_next_char => {
                 if in_single_quotes {
                     current_token.push(character);
                 } else if in_double_quotes {
                     if let Some(next_character) = characters.peek() {
                         match next_character {
-                            '"' | '\\' => escape_next_character = true,
+                            '"' | '\\' => escape_next_char = true,
                             _ => current_token.push(character),
                         }
                     }
                 } else {
-                    escape_next_character = true;
+                    escape_next_char = true;
                 }
             },
-            character if character.is_whitespace() && !escape_next_character => {
+            file_descriptor if file_descriptor == '1' && !escape_next_char && !in_single_quotes && !in_double_quotes => {
+                if let Some(next_character) = characters.peek() {
+                    if *next_character == '>' {
+                        in_stdout_redirection = true;
+                        characters.next();
+                    } else {
+                        current_token.push(file_descriptor);
+                    }
+                }
+            },
+            file_descriptor if file_descriptor == '2' && !escape_next_char && !in_single_quotes && !in_double_quotes => {
+                if let Some(next_character) = characters.peek() {
+                    if *next_character == '>' {
+                        in_stderr_redirection = true;
+                        characters.next();
+                    } else {
+                        current_token.push(file_descriptor);
+                    }
+                }
+            },
+            redirect_operator if redirect_operator == '>' && in_stdout_redirection => {
+                stdout.append_to = true;
+            },
+            redirect_operator if redirect_operator == '>' && in_stderr_redirection => {
+                stderr.append_to = true;
+            }
+            redirect_operator if redirect_operator == '>' && !in_stdout_redirection && !escape_next_char && !in_single_quotes && !in_double_quotes => {
+                in_stdout_redirection = true;
+            },
+            character if character.is_whitespace() && !escape_next_char => {
                 if in_single_quotes || in_double_quotes {
                     current_token.push(character);
                 } else if !current_token.is_empty() {
-                    tokens.push(current_token);
+                    if in_stdout_redirection {
+                        stdout.file_name = Some(current_token);
+                        in_stdout_redirection = false;
+                    } else if in_stderr_redirection {
+                        stderr.file_name = Some(current_token);
+                        in_stderr_redirection = false;
+                    } else {
+                        tokens.push(current_token);
+                    }
                     current_token = String::new();
                 }
             },
             _ => {
                 current_token.push(character);
-                escape_next_character = false;
+                escape_next_char = false;
             },
         }
     }
 
-    if !current_token.is_empty() { tokens.push(current_token); }
+    if !current_token.is_empty() {
+        if in_stdout_redirection {
+            stdout.file_name = Some(current_token);
+        } else if in_stderr_redirection {
+            stderr.file_name = Some(current_token);
+        } else {
+            tokens.push(current_token);
+        }
+    }
 
-    tokens
+    ParsedCommand {
+        tokens: if tokens.is_empty() { None } else { Some(tokens) },
+        stdout,
+        stderr,
+    }
+}
+
+fn get_output_redirection(output: OutputRedirection) -> Box<dyn Write> {
+    match output.file_name {
+        Some(file_name) => {
+            let file = if output.append_to {
+                OpenOptions::new().append(true).create(true).open(file_name).unwrap()
+            } else {
+                File::create(file_name).unwrap()
+            };
+            let writer = io::BufWriter::new(file);
+            Box::new(writer)
+
+        },
+        None => {
+            match output.output_type {
+                OutputType::STDOUT => Box::new(io::stdout()),
+                OutputType::STDERR => Box::new(io::stderr()),
+            }
+        },
+    }
 }
